740bce604f9a137fa29d4c773558a3df57e3bca3
DarekDGB <majescyk@gmail.com>
2025-11-24 12:06:01 +0000

    Update test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,20 +1,4 @@
-import importlib
-import os
-import sys
-
-
-# Make sure Python can see the project root
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if ROOT_DIR not in sys.path:
-    sys.path.insert(0, ROOT_DIR)
-
-
-def test_import_dqsn_modules():
-    """
-    Basic sanity check: the core DQSN modules must be importable.
-    """
-    dqsn_core = importlib.import_module("dqsn_core")
-    dqsn_engine = importlib.import_module("dqsn_engine")
-
-    assert dqsn_core is not None
-    assert dqsn_engine is not None
+def test_imports():
+    # Just make sure the package and modules can be imported
+    import dqsnetwork.dqsn_core
+    import dqsnetwork.dqsn_engine
