03de915c46e04c38ea72e873ec950ff0419e8178
DarekDGB <majescyk@gmail.com>
2026-01-10 17:29:10 +0000

    chore: move dqsn_core/dqsn_engine prototypes to legacy package

---
+++ legacy/__init__.py
@@ -0,0 +1,6 @@
+"""
+Legacy modules preserved for historical reference.
+
+These modules are NOT part of the Shield Contract v3 authority surface.
+Authoritative v3 entrypoint: dqsnetwork.v3.DQSNV3
+"""
